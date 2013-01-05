bfe5316be68d457343bf3c07e6f97fc2b1df4e9d
Joshua Estes <Joshua.Estes@ScenicCityLabs.com>
2013-01-05 03:47:07 -0600

    added bitcoind:getinfo command

---
+++ Command/BitcoindGetinfoCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Nbobtc\Bundle\BitcoindBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class BitcoindGetinfoCommand extends ContainerAwareCommand
+{
+
+    protected function configure()
+    {
+        $this
+            ->setName('bitcoind:getinfo')
+            ->setDescription('Run the getinfo command')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $container = $this->getContainer();
+        $bitcoind  = $container->get('bitcoind');
+        $info = $bitcoind->getinfo();
+
+        $output->writeln(array(
+            sprintf('Version:          %s',$info->version),
+            sprintf('Protocol Version: %s',$info->protocolversion),
+            sprintf('Wallet Version:   %s',$info->walletversion),
+            sprintf('Balance:          %s',$info->balance),
+            sprintf('Blocks:           %s',$info->blocks),
+            sprintf('Connections:      %s',$info->connections),
+            sprintf('Proxy:            %s',$info->proxy),
+            sprintf('Difficulty:       %s',$info->difficulty),
+            sprintf('Testnet           %s',$info->testnet),
+            sprintf('Keypool Oldest:   %s',$info->keypoololdest),
+            sprintf('Keypool Size:     %s',$info->keypoolsize),
+            sprintf('TX Fee:           %s',$info->paytxfee),
+            sprintf('Errors:           %s',$info->errors),
+        ));
+    }
+
+}
