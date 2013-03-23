8c0c582b6dee02d57153a2335ed897278feaad76
Joshua Estes <Joshua.Estes@ScenicCityLabs.com>
2013-03-22 23:12:32 -0500

    adding some new commands that do not do anything right now

---
+++ Command/BitcoindGethashespersecCommand.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Nbobtc\Bundle\BitcoindBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class BitcoindGethashespersecCommand extends ContainerAwareCommand
+{
+
+    protected function configure()
+    {
+        $this
+            ->setName('bitcoind:gethashespersec')
+            ->setDescription('')
+            ->setDefinition(array(
+            ))
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $container = $this->getContainer();
+        $bitcoind  = $container->get('bitcoind');
+    }
+
+}
+
+
